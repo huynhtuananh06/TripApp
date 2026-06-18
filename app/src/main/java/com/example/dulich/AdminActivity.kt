8fec16b082f75b837d30c8a16cd46ebfe2447dee
Tuấn Anh <tuananh.1706006@gmail.com>
2026-06-18 10:47:40 +0700

    quan ly user

--- app/src/main/java/com/example/dulich/AdminActivity.kt
+++ app/src/main/java/com/example/dulich/AdminActivity.kt
@@ -1,17 +1,27 @@
 package com.example.dulich
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class AdminActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.decorView.systemUiVisibility =
+        window.getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        )
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        // CARD USERS
+        val cardUsers = findViewById<CardView?>(R.id.cardUsers)
+
+        cardUsers.setOnClickListener(View.OnClickListener { v: View? ->
+            val intent = Intent(this@AdminActivity, UsersActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
